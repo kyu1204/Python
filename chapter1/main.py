5b140aa13cc82ace3aa3332909ea33cae1bc846c
MINT <pb1123love@gmail.com>
2024-01-04 22:55:20 +0900

    chapter 1

---
+++ chapter1/main.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+a = 10
+b = 1.1
+c = 'abc'
+d = 'a'
+e = True or False
+f = a + b
+n = None
+
+print(f"a: {a}, b: {b}, c: {c}, d: {d}, e: {e}")
+print(f"f: {f}")
+
+c = d + str(a)
+print(c)
+
+# null check
+none_check = a is None and a == 10
+
+print("메뉴를 고르세요")
+print("1. 감자")
+print("2. 토마토")
+choice = input("선택: ")
+print(f"choice: {choice}")
+
